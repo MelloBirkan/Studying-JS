6afd4b183001369c0bb117930a098917bbbf91c0
MelloBirkan <82703050+MelloBirkan@users.noreply.github.com>
2023-07-18 23:32:58 +0000

    Initial commit

---
+++ dogYears.js
@@ -0,0 +1,12 @@
+let myAge = 23;
+let earlyYears = 2;
+
+earlyYears *= 10.5;
+let laterYears = myAge - 2;
+laterYears *= 4;
+
+const myAgeInDogYears = laterYears + earlyYears;
+
+const myName = 'Marcello'.toLowerCase();
+
+console.log(`My name is ${myName}. I am ${myAge} years old in human years which is ${myAgeInDogYears} years old in dog years.`);
