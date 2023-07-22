f12a8d194ea133cf39b3de0a26ce04cf048e9a41
MelloBirkan <82703050+MelloBirkan@users.noreply.github.com>
2023-07-22 05:22:12 +0000

    New nano project

---
+++ magicEightBall.js
@@ -0,0 +1,44 @@
+let userName = "Mello";
+
+userName ? userName : (userName = "Mr Stranger");
+console.log(`Hello ${userName}!`);
+
+const userQuestion = "Am i the fastest person in the world ?";
+
+console.log(`${userName} asks: ${userQuestion}`);
+
+const randomNumber = Math.floor(Math.random() * 8);
+
+let eightBall = "";
+
+switch (randomNumber) {
+  case 0:
+    eightBall = "It is certain";
+    break;
+  case 1:
+    eightBall = "It is decidedly so";
+    break;
+  case 2:
+    eightBall = "Reply hazy try again";
+    break;
+  case 3:
+    eightBall = 'Cannot predict now';
+    break;
+  case 4:
+    eightBall = 'Do not count on it';
+    break;
+  case 5:
+    eightBall = 'My sources say no';
+    break;
+  case 6:
+    eightBall = "Reply hazy try again";
+    break;
+  case 7:
+    eightBall = 'Outlook not so good';
+    break;
+  case 8:
+    eightBall = 'Signs point to yes';
+    break;
+}
+
+console.log(eightBall);
